f309be68848fbfd486b543ee3691cd93a6fe6ae6
!Gustavo! <72351688+GustavoGLD@users.noreply.github.com>
2021-07-30 20:47:11 -0300

    Update asteroid.hpp

--- Source/Entity/asteroid.hpp
+++ Source/Entity/asteroid.hpp
@@ -15,14 +15,8 @@ class Asteroid : public Handler
 
         sf::CircleShape asteroid_shape;
         sf::Texture     asteroid_texture;
-        sf::Texture     asteroid_texture_destroying[4];
     
         float angle     = 0.0f;
-
-        uint8_t frames_anim   = 0;
-        uint8_t current_anim  = 0;
-        uint8_t max_anim      = 3;
-        uint8_t anim_speed    = 20;
 
         Asteroid();
         void tick(float delta_time, sf::RenderWindow &window);
@@ -33,11 +27,6 @@ Asteroid::Asteroid()
 {
 
     this->type = ASTEROID;
-
-
-    for(uint8_t px = 0; px <= 4; px++){
-        asteroid_texture_destroying[px].loadFromFile("Resources/destroying/asteroid" + std::to_string(px) + ".png");
-    }
 
     /** RANDOMIZE **/
     std::srand(time(NULL));
@@ -59,7 +48,7 @@ Asteroid::Asteroid()
     //std::cout << random_number_to_spawn << "\n";
 
     this->radius    = 22.0f;
-    this->velocity  = 14.0f;
+    this->velocity  = 140.0f;
 
     if(!asteroid_texture.loadFromFile("Resources/asteroide.png"))
     {
@@ -69,6 +58,7 @@ Asteroid::Asteroid()
 
     asteroid_shape.setPosition(sf::Vector2f( this->dir_x, this->dir_y ));
     asteroid_shape.setRadius(this->getRadius());
+    asteroid_shape.setTexture(&asteroid_texture);
 }
 
 void Asteroid::tick(float delta_time, sf::RenderWindow &window)
@@ -97,30 +87,10 @@ void Asteroid::tick(float delta_time, sf::RenderWindow &window)
     else if(this->posX < -40)
         asteroid_shape.setPosition(sf::Vector2f(this->posX + window_x, this->posY));
     
-    if(!this->isDestroyed){
-        frames_anim++;
-        if(frames_anim >= anim_speed){
-            frames_anim = 0;
-            current_anim++;
-            if(current_anim >= max_anim){
-                current_anim = 0;
-            }
-        }
-    }else{
-        frames_anim  = 0;
-        current_anim = 0;
-    }
-
 }
 
 void Asteroid::render(sf::RenderWindow &window)
 {
-    if(!this->isDestroyed){
-        asteroid_shape.setTexture(&asteroid_texture_destroying[current_anim]);
-    }else{
-        asteroid_shape.setTexture(&asteroid_texture);
-    }
-
     window.draw(asteroid_shape);
 }
 
