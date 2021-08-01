5b3faf603e00a54f3369637db990420755101bba
!Gustavo! <72351688+GustavoGLD@users.noreply.github.com>
2021-08-01 14:44:51 -0300

    LifeBar Added

--- Source/Main/menu.cpp
+++ Source/Main/menu.cpp
@@ -1,5 +1,7 @@
-#include <iostream>
+#include <bits/stdc++.h>
 #include <SFML/Graphics.hpp>
+#include "../Entity/handler.hpp"
+
 
 class MainMenu
 {
@@ -71,6 +73,25 @@ class MainMenu
             }
         }
 };
+
+struct vec2i { int x, y; };
+vec2i lifebar_size = { 200, 20 };
+void SHOW_LIFEBAR(Handler* obj, std::pair<uint, uint> position, sf::RenderWindow &window){
+
+    sf::RectangleShape outline(sf::Vector2f(lifebar_size.x, lifebar_size.y));
+    outline.setPosition(*new sf::Vector2f(position.first, position.second));
+    outline.setFillColor(sf::Color::Transparent);
+    outline.setOutlineColor(sf::Color::Cyan);
+    outline.setOutlineThickness(5.0f);
+    window.draw(outline);
+    
+
+    if (obj->life <= 0) return;
+    sf::RectangleShape lifebar(sf::Vector2f(lifebar_size.x * (obj->life / 100), lifebar_size.y));
+    lifebar.setPosition(*new sf::Vector2f(position.first, position.second));
+    lifebar.setFillColor(sf::Color::Red);
+    window.draw(lifebar);
+}
 
 MainMenu *main_menu;
 
