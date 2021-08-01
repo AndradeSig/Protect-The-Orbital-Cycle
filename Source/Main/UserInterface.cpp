119e4635f80d8275e29580f60abaf5614aed411c
!Gustavo! <72351688+GustavoGLD@users.noreply.github.com>
2021-08-01 14:48:59 -0300

    Update UserInterface.cpp

--- Source/Main/UserInterface.cpp
+++ Source/Main/UserInterface.cpp
@@ -29,3 +29,22 @@ void SHOW_GUI_FPS(std::string str_fps ,sf::RenderWindow &window)
     fps_text.setString(str_fps);
     window.draw(fps_text);
 }
+
+struct vec2i { int x, y; };
+vec2i lifebar_size = { 200, 20 };
+void SHOW_LIFEBAR(Handler* obj, std::pair<uint, uint> position, sf::RenderWindow &window){
+
+    sf::RectangleShape outline(sf::Vector2f(lifebar_size.x, lifebar_size.y));
+    outline.setPosition(*new sf::Vector2f(position.first, position.second));
+    outline.setFillColor(sf::Color::Transparent);
+    outline.setOutlineColor(sf::Color::Cyan);
+    outline.setOutlineThickness(5.0f);
+    window.draw(outline);
+    
+
+    if (obj->life <= 0) return;
+    sf::RectangleShape lifebar(sf::Vector2f(lifebar_size.x * (obj->life / 100), lifebar_size.y));
+    lifebar.setPosition(*new sf::Vector2f(position.first, position.second));
+    lifebar.setFillColor(sf::Color::Red);
+    window.draw(lifebar);
+}
